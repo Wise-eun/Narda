2a7e57ef933ca21f649a5d39281c9fb1bd619d7f
Wise-eun <hee1871@naver.com>
2023-03-31 13:06:57 +0900

    [Feat] 로그인 화면 구현

---
+++ lib/signup_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class SignUpScreen extends StatelessWidget {
+  const SignUpScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      body:Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("회원가입 화면입니다."),
+            SizedBox(
+              height: 10,
+            ),
+            TextButton(
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.white)
+                ),
+                onPressed: (){
+                  Navigator.pop(context);
+                },
+                child: Text("돌아가기"))
+          ],
+        ),
+      )
+
+      );
+  }
+}
