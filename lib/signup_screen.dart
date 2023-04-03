8a4b6ad7038500e3e83bcf6d1afefdf74ddb436b
Wise-eun <hee1871@naver.com>
2023-04-04 01:32:48 +0900

    [Feat] 회원가입구현

--- lib/signup_screen.dart
+++ lib/signup_screen.dart
@@ -1,6 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/services.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:fluttertoast/fluttertoast_web.dart';
 
 class SignupScreen extends StatefulWidget {
   const SignupScreen({Key? key}) : super(key: key);
@@ -13,17 +16,67 @@ class _SignupScreenState extends State<SignupScreen> {
   final _idTextEditController = TextEditingController();
   final _pwTextEditController = TextEditingController();
   final _pwCheckTextEditController = TextEditingController();
+  final _phoneTextEditController = TextEditingController();
 
-
-@override
+final db = FirebaseFirestore.instance;
+  bool checkID = false;
+  @override
   void dispose() {
     // TODO: implement dispose
   _idTextEditController.dispose();
   _pwTextEditController.dispose();
   _pwCheckTextEditController.dispose();
+  _phoneTextEditController.dispose();
     super.dispose();
   }
 
+  void createUserData(String id, String phone_num, String pw)
+  {
+final userCollectionReference = db.collection("user").doc(id);
+userCollectionReference.set({
+  "id":id,
+  "phone_num":phone_num,
+  "pw":pw,
+});
+  }
+
+  void ShowToastMessage(String msg)
+  {
+   Fluttertoast.showToast(
+       msg: msg,
+     gravity: ToastGravity.BOTTOM,
+     backgroundColor: Colors.redAccent,
+     fontSize: 20,
+     textColor: Colors.white,
+     toastLength: Toast.LENGTH_SHORT
+   );
+  }
+
+
+  bool checkUserID(String id)
+  {
+
+
+
+    final docRef = db.collection("user").doc(id);
+    docRef.get().then(
+        (DocumentSnapshot doc){
+          final data = doc.data() as Map<String,dynamic>;
+
+          if(data.isNotEmpty)
+          {
+            return true;
+          }
+          else
+          {
+            return false;
+          }
+        },
+    );
+return false;
+
+
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -40,10 +93,11 @@ class _SignupScreenState extends State<SignupScreen> {
               ),
 
               Container(
-
                 height: 45,
                 margin: EdgeInsets.only(left:50,right:50),
-                child:    TextField(
+                child:
+                TextField(
+                  controller: _idTextEditController,
                   decoration: InputDecoration(
                     border: OutlineInputBorder(),
                     labelText: '아이디',
@@ -59,7 +113,17 @@ class _SignupScreenState extends State<SignupScreen> {
                       backgroundColor: MaterialStateProperty.all(Colors.white)
                   ),
                   onPressed: (){
-                    Navigator.pop(context);
+                        if(checkUserID(_idTextEditController.text))
+                          {
+                            ShowToastMessage("사용할 수 없는 아이디입니다.");
+                            checkID = false;
+                          }
+                          else
+                            {
+                              ShowToastMessage("사용할 수 있는 아이디입니다.");
+                              checkID = true;
+
+                            }
                   },
                   child: Text("중복확인"))
               ,
@@ -70,7 +134,9 @@ class _SignupScreenState extends State<SignupScreen> {
               Container(
                 height: 45,
                 margin: EdgeInsets.only(left:50,right:50),
-                child:    TextField(
+                child:
+                TextField(
+                  controller: _pwTextEditController,
                   decoration: InputDecoration(
                     border: OutlineInputBorder(),
                     labelText: '비밀번호',
@@ -84,14 +150,30 @@ class _SignupScreenState extends State<SignupScreen> {
               Container(
                 height: 45,
                 margin: EdgeInsets.only(left:50,right:50),
-                child:    TextField(
+                child:
+                TextField(
+                  controller: _pwCheckTextEditController,
                   decoration: InputDecoration(
                     border: OutlineInputBorder(),
                     labelText: '비밀번호 확인',
                   ),
                 ),
               ),
-
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                height: 45,
+                margin: EdgeInsets.only(left:50,right:50),
+                child:
+                TextField(
+                  controller: _phoneTextEditController,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: '휴대폰 번호',
+                  ),
+                ),
+              ),
               SizedBox(
                 height: 30,
               ),
@@ -100,7 +182,28 @@ class _SignupScreenState extends State<SignupScreen> {
                       backgroundColor: MaterialStateProperty.all(Colors.white)
                   ),
                   onPressed: (){
-                    Navigator.pop(context);
+                    if(_pwTextEditController.text.compareTo(_pwCheckTextEditController.text) == 0 )
+                    {
+
+                      print("비밀번호 서로 같음");
+                      //id 중복확인
+                      bool checkID = checkUserID(_idTextEditController.text);
+if(checkID)
+  {
+    createUserData(_idTextEditController.text,_phoneTextEditController.text,
+        _pwTextEditController.text);
+  }
+else
+  {
+ShowToastMessage("아이디 중복확인을 해주세요");
+  }
+
+
+                    }
+                    else
+                    {
+                      print("비밀번호 서로 안같음");
+                    }
                   },
                   child: Text("회원가입"))
               ,
