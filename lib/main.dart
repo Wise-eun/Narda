6dcca463a0b98514df73c201fd57ef0733728676
Wise-eun <hee1871@naver.com>
2023-04-03 23:47:01 +0900

    [Style] Stateless -> Stateful 변경

--- lib/main.dart
+++ lib/main.dart
@@ -28,83 +28,90 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue
       ),
-      home: LoginScreen(),
+      home: Loginscreen(),
     );
   }
 }
+class Loginscreen extends StatefulWidget {
+  const Loginscreen({Key? key}) : super(key: key);
 
-class LoginScreen extends StatelessWidget {
-  //const LoginScreen({Key? key}) : super(key: key);
+  @override
+  _LoginscreenState createState() => _LoginscreenState();
+}
 
+class _LoginscreenState extends State<Loginscreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
+        body: Center(
 //tee
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              height: 45,
-              margin: EdgeInsets.only(left:50,right:50),
-              child:    TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: '아이디',
-                ),
-              ),
-            ),
-
-            SizedBox(
-              height: 10,
-            ),
-            Container(
-              height: 45,
-              margin: EdgeInsets.only(left:50,right:50),
-              child:    TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: '패스워드',
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            TextButton(
-                style: ButtonStyle(
-                    backgroundColor:  MaterialStateProperty.all(Colors.grey[350]),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  height: 45,
+                  margin: EdgeInsets.only(left:50,right:50),
+                  child:    TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: '아이디',
+                    ),
+                  ),
                 ),
 
-                onPressed: (){
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const MainScreen()),
-                  );
-                },
-                child: Text('로그인')),
-            SizedBox(
-              height: 10,
-            ),
-            TextButton(
-                style: ButtonStyle(
-                    backgroundColor:  MaterialStateProperty.all(Colors.grey[350])
+                SizedBox(
+                  height: 10,
                 ),
-                onPressed: (){
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const SignUpScreen()),
-                  );},
-                child: Text('회원가입')),
-            SizedBox(
-              height: 10,
-            ),
-          ],
+                Container(
+                  height: 45,
+                  margin: EdgeInsets.only(left:50,right:50),
+                  child:    TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: '패스워드',
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                TextButton(
+                    style: ButtonStyle(
+                      backgroundColor:  MaterialStateProperty.all(Colors.grey[350]),
+                    ),
+
+                    onPressed: (){
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => const MainScreen()),
+                      );
+                    },
+                    child: Text('로그인')),
+                SizedBox(
+                  height: 10,
+                ),
+                TextButton(
+                    style: ButtonStyle(
+                        backgroundColor:  MaterialStateProperty.all(Colors.grey[350])
+                    ),
+                    onPressed: (){
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) =>  SignupScreen()),
+                      );},
+                    child: Text('회원가입')),
+                SizedBox(
+                  height: 10,
+                ),
+              ],
+            )
+
         )
-
-     )
     );
   }
 }
+
+
+
 
 
--- lib/signup_screen.dart
+++ lib/signup_screen.dart
@@ -2,34 +2,127 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/services.dart';
 
+class SignupScreen extends StatefulWidget {
+  const SignupScreen({Key? key}) : super(key: key);
 
-class SignUpScreen extends StatelessWidget {
-  const SignUpScreen({Key? key}) : super(key: key);
+  @override
+  _SignupScreenState createState() => _SignupScreenState();
+}
+
+class _SignupScreenState extends State<SignupScreen> {
+  final _idTextEditController = TextEditingController();
+  final _pwTextEditController = TextEditingController();
+  final _pwCheckTextEditController = TextEditingController();
+
+
+@override
+  void dispose() {
+    // TODO: implement dispose
+  _idTextEditController.dispose();
+  _pwTextEditController.dispose();
+  _pwCheckTextEditController.dispose();
+    super.dispose();
+  }
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue,
-      body:Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("회원가입 화면입니다."),
-            SizedBox(
-              height: 10,
-            ),
-            TextButton(
-                style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(Colors.white)
-                ),
-                onPressed: (){
-                  Navigator.pop(context);
-                },
-                child: Text("돌아가기"))
-          ],
-        ),
-      )
 
-      );
+        backgroundColor: Colors.pink[100],
+        body:Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("회원가입 화면입니다."),
+              SizedBox(
+                height: 10,
+              ),
+
+              Container(
+
+                height: 45,
+                margin: EdgeInsets.only(left:50,right:50),
+                child:    TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: '아이디',
+                  ),
+                ),
+              ),
+
+              SizedBox(
+                height: 10,
+              ),
+              TextButton(
+                  style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.white)
+                  ),
+                  onPressed: (){
+                    Navigator.pop(context);
+                  },
+                  child: Text("중복확인"))
+              ,
+              SizedBox(
+                height: 15,
+              )
+              ,
+              Container(
+                height: 45,
+                margin: EdgeInsets.only(left:50,right:50),
+                child:    TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: '비밀번호',
+                  ),
+                ),
+              ),
+
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                height: 45,
+                margin: EdgeInsets.only(left:50,right:50),
+                child:    TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: '비밀번호 확인',
+                  ),
+                ),
+              ),
+
+              SizedBox(
+                height: 30,
+              ),
+              TextButton(
+                  style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.white)
+                  ),
+                  onPressed: (){
+                    Navigator.pop(context);
+                  },
+                  child: Text("회원가입"))
+              ,
+              SizedBox(
+                height: 10,
+              ),
+              TextButton(
+                  style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.white)
+                  ),
+                  onPressed: (){
+                    Navigator.pop(context);
+                  },
+                  child: Text("돌아가기"))
+            ],
+          ),
+        )
+
+
+
+    );
   }
 }
+
+
