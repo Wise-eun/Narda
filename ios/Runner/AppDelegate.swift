d2e74bed660f4dceb71ebf6402bf829ea3f689bb
tilkoas35 <tilkoas35@naver.com>
2023-04-11 00:44:32 +0900

    [Feat] T MAP ios 구현

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,48 @@
 import UIKit
 import Flutter
+import TMapSDK
 
 @UIApplicationMain
-@objc class AppDelegate: FlutterAppDelegate {
+@objc class AppDelegate: FlutterAppDelegate,  TMapTapiDelegate{
+let appKey:String = "yIvsQzTPnWa2bnrbh6HeN9iq4CbOhadO3M3g46RT";
+
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+  let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+  let channel = FlutterMethodChannel(name:"mobile/parameters", binaryMessenger:
+  controller.binaryMessenger)
+
+  channel.setMethodCallHandler({
+  [weak self]  (call:FlutterMethodCall, result : FlutterResult) -> Void in
+
+  switch (call.method){
+  case "initTmapAPI":
+  self?.initTmapAPI()
+  result("initTmapAPI")
+  break;
+  case "isTmapApplicationInstalled":
+  if(TMapApi.isTmapApplicationInstalled())
+  {
+  result("")
+  }
+  else
+  {
+  let url = TMapApi.getTMapDownUrl()
+  result(url)
+  }
+  break;
+  default:
+  result(FlutterMethodNotImplemented)
+  break;
+  }
+  })
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+  private func initTmapAPI(){
+  TMapApi.setSKTMapAuthenticationWithDelegate(self,apiKey:appKey)
   }
 }
--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,11 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
   const MainScreen({Key? key}) : super(key: key);
 
   @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  static const MethodChannel _methodChannel = MethodChannel('mobile/parameters');
+
+  Future<void> _initTmapAPI() async{
+    try{
+      final String result = await _methodChannel.invokeMethod('initTmapAPI');
+      print('initTmapAPI result : $result');
+    }on PlatformException{
+      print('PratformException');
+    }
+  }
+  @override
+  void initState() {
+    // TODO: implement initState
+    _initTmapAPI();
+  }
+
+  Future<void> _tmapCheck() async {
+    try {
+      final String result = await _methodChannel.invokeMethod(
+          'isTmapApplicationInstalled');
+      print('is TmapApplicationInstalled result : $result');
+
+      if (result.isEmpty || result.length == 0) {
+        String url = "${Uri.encodeFull(
+            "https://apis.openapi.sk.com/tmap/app/routes?appKey=yIvsQzTPnWa2bnrbh6HeN9iq4CbOhadO3M3g46RT&name=SKT타워&lon=126.984098&lat=37.566385")}";
+        if (await canLaunch(url)) {
+          print("설치");
+          await launch(url, forceSafariVC: false, forceWebView: false);
+        }
+        else {
+          if (await canLaunch(result)) {
+            print("미설치되어서 설치페이지로 이동");
+            await launch(result, forceSafariVC: false, forceWebView: false);
+          }
+        }
+      }
+    }
+    on PlatformException {
+      print('PlatformException');
+    }
+  }
+
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return  Scaffold(
         backgroundColor: Colors.blue[200],
         body:Center(
           child: Column(
@@ -20,9 +70,10 @@ class MainScreen extends StatelessWidget {
                       backgroundColor: MaterialStateProperty.all(Colors.white)
                   ),
                   onPressed: (){
-                    Navigator.pop(context);
+                   // Navigator.pop(context);
+                    _tmapCheck();
                   },
-                  child: Text("로그인으로 돌아가기"))
+                  child: Text("지도"))
             ],
           ),
         )
@@ -30,3 +81,6 @@ class MainScreen extends StatelessWidget {
     );
   }
 }
+
+
+
