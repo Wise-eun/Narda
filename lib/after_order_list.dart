01d5382728ddeaf6e8e2c24fda970bf50fa91858
dodms <dodms1006@naver.com>
2023-05-22 20:17:10 +0900

    [Feat] 진행 중, 완료 오더 리스트 화면 재구성

--- lib/after_order_list.dart
+++ lib/after_order_list.dart
@@ -5,6 +5,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:http/http.dart' as http;
+import 'package:percent_indicator/linear_percent_indicator.dart';
 //import 'package:percent_indicator/circular_percent_indicator.dart';
 
 import 'api/api.dart';
@@ -23,7 +24,9 @@ const kLabels = ["Edit Profile", "Accounts"];
 const kTabBgColor = Color(0xFF8F32A9);
 const kTabFgColor = Colors.white;
 
-List<OrderDetail> orders = [];
+List<OrderDetail> proceedingOrders = [];
+List<OrderDetail> completeOrders = [];
+
 
 class AfterOrderListScreen extends StatefulWidget {
   // const ListviewPage({Key? key, required this.orders}) : super(key: key);
@@ -38,195 +41,386 @@ class AfterOrderListScreen extends StatefulWidget {
 class _AfterOrderListScreenState extends State<AfterOrderListScreen>
     with SingleTickerProviderStateMixin {
   late TabController _tabController;
-
+  bool order_state = true;
+  bool previous = false;
+  var _listView;
   String tabValue = "진행 중";
   List<Tab> _tabs = [
     Tab(text: '진행 중 ()'),
     Tab(text: '완료 ()'),
   ];
 
-
   @override
   void initState() {
     // TODO: implement initState
-    orderList();
+    proceedingOrderList();
+    completeOrderList();
     _tabController =
         TabController(length: 2, vsync: this, animationDuration: Duration.zero);
     super.initState();
   }
 
-  orderList() async {
+  proceedingOrderList() async {
     try {
       var response = await http.post(Uri.parse(API.afterOrderList),
-          body: {'userId': widget.userId, 'state': tabValue});
+          body: {'userId': widget.userId, 'state': "진행 중"});
       if (response.statusCode == 200) {
-        orders = [];
+        proceedingOrders = [];
         var responseBody = jsonDecode(response.body);
         if (responseBody['success'] == true) {
           print("오더 리스트 불러오기 성공");
 
           List<dynamic> responseList = responseBody['userData'];
           for (int i = 0; i < responseList.length; i++) {
-            print(OrderDetail.fromJson(responseList[i]));
-            orders.add(OrderDetail.fromJson(responseList[i]));
+            //print(OrderDetail.fromJson(responseList[i]));
+            proceedingOrders.add(OrderDetail.fromJson(responseList[i]));
           }
         } else {
           print("오더 리스트 불러오기 실패");
         }
         _tabs = [
-          Tab(text: '진행 중 (${orders.length})'),
-          Tab(text: '완료 (${orders.length})'),
+          Tab(text: '진행 중 (${proceedingOrders.length})'),
+          Tab(text: '완료 (${completeOrders.length})'),
         ];
 
         setState(() {});
-        return orders;
+        return proceedingOrders;
       }
     } catch (e) {
       print(e.toString());
     }
   }
 
-  @override
-  Widget build(BuildContext context) {
-    var _listView = ListView.separated(
-      padding: const EdgeInsets.all(8),
-      itemCount: orders.length,
+  completeOrderList() async {
+    try {
+      var response = await http.post(Uri.parse(API.afterOrderList),
+          body: {'userId': widget.userId, 'state': "완료"});
+      if (response.statusCode == 200) {
+        completeOrders = [];
+        var responseBody = jsonDecode(response.body);
+        if (responseBody['success'] == true) {
+          print("신규 오더 리스트 불러오기 성공");
+
+          List<dynamic> responseList = responseBody['userData'];
+          for (int i = 0; i < responseList.length; i++) {
+            //print(OrderDetail.fromJson(responseList[i]));
+            if(!previous){
+              print("${responseList[i]['deliveryTime']}  ${responseList[i]['orderId']}");
+              if(responseList[i]['deliveryTime'] != null){
+                if(responseList[i]['deliveryTime'].toString().substring(0,10) == DateTime.now().toString().substring(0, 10)){
+                  completeOrders.add(OrderDetail.fromJson(responseList[i]));
+                }
+              }
+
+            }
+            else{
+              completeOrders.add(OrderDetail.fromJson(responseList[i]));
+            }
+          }
+        } else {
+          //print("오더 리스트 불러오기 실패");
+        }
+        _tabs = [
+          Tab(text: '진행 중 (${proceedingOrders.length})'),
+          Tab(text: '완료 (${completeOrders.length})'),
+        ];
+
+        print("탭 텍스트 설정");
+
+        setState(() {});
+        return completeOrders;
+      }
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  proceeding() {
+    _listView = ListView.separated(
+      itemCount: proceedingOrders.length,
       itemBuilder: (BuildContext context, int index) {
         String payment, elapsedTime;
         String state;
         String storeDong = "";
         String deliveryDong = "";
 
-        if (orders[index].payment == 0)
+        if (proceedingOrders[index].payment == 0)
           payment = "선결제";
-        else if (orders[index].payment == 1)
-          payment = "현장결제(카드)";
+        else if (proceedingOrders[index].payment == 1)
+          payment = "현장결제 / 카드";
         else
-          payment = "현장결제(현금)";
+          payment = "현장결제 / 현금";
 
-        if (orders[index].state == 2) {
+        if (proceedingOrders[index].state == 2) {
           state = "픽업 전";
-        } else if (orders[index].state == 3) {
+        } else if (proceedingOrders[index].state == 3) {
           state = "배달 중";
         } else {
-          state = "${orders[index].deliveryFee}원";
+          state = "${proceedingOrders[index].deliveryFee}원";
         }
 
-        final storeLocationList = orders[index].storeLocation.split(' ');
+        final storeLocationList = proceedingOrders[index].storeLocation.split(' ');
         storeLocationList.forEach((element) {
-          if(element[element.length-1] == '동'){
+          if (element[element.length - 1] == '동') {
             storeDong = element;
           }
         });
 
-        final deliveryLocationList = orders[index].deliveryLocation.split(' ');
+        final deliveryLocationList = proceedingOrders[index].deliveryLocation.split(' ');
         deliveryLocationList.forEach((element) {
-          if(element[element.length-1] == '동'){
+          if (element[element.length - 1] == '동') {
             deliveryDong = element;
           }
         });
 
-        DateTime orderTime = DateTime.parse(orders[index].orderTime);
+        DateTime orderTime = DateTime.parse(proceedingOrders[index].orderTime);
 
         DateTime current = DateTime.now();
         Duration duration = current.difference(orderTime);
-        double percent = (duration.inMinutes/orders[index].predictTime).toDouble();
+        double _percent =
+            (duration.inMinutes / proceedingOrders[index].predictTime).toDouble();
+        if (_percent >= 1) _percent = 1;
 
-        print(duration.inMinutes.toString() + " // " + orders[index].predictTime.toString());
-        print(orders[index].orderId);
-        print(current.toString() + " " + orderTime.toString());
-        print("percent : " + percent.toString());
+        //(duration.inMinutes.toString() + " // " + orders[index].predictTime.toString());
+        //print(orders[index].orderId);
+        //print(current.toString() + " " + orderTime.toString());
+        //print("percent : " + _percent.toString());
 
-        return Card(
-          child: ListTile(
-            //visualDensity: VisualDensity(horizontal: 0, vertical: 0),
-            contentPadding: EdgeInsets.only(left:15, right: 15, top: 0, bottom: 0),
-            leading: Column(
+        return Container(
+            child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: <Widget>[
-                  Text(
-                      "${orders[index].orderTime.substring(10, 16)}  ${orders[index].deliveryDistance.toStringAsFixed(2)}km"),
-                  Text(
-                    "${orders[index].storeName}",
-                    style: TextStyle(fontSize: 20),
-                  ),
-                  Text("${storeDong} > ${deliveryDong}"),
-                ]),
-            subtitle: Text(""),
-            trailing: Column(children: <Widget>[
-              Text(state, style: TextStyle(color: Colors.red),),
-              //Text("${duration.inMinutes}분"),
-              CircularProgressIndicator(
-                value: percent,
-                color: Colors.red,
-                backgroundColor: Color(0xffE3E5EA),
-                strokeWidth : 6.0,
+              Container(
+                margin: EdgeInsets.all(10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                            decoration: BoxDecoration(
+                                color: Color(0xfff1f2f3),
+                                borderRadius: BorderRadius.circular(10)),
+                            child: Container(
+                              padding: EdgeInsets.all(5),
+                              child: Text(
+                                "${storeDong} > ${deliveryDong}  |  ${proceedingOrders[index].deliveryDistance.toStringAsFixed(2)}km",
+                                style: TextStyle(fontSize: 12),
+                              ),
+                            )),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Text(
+                          "${proceedingOrders[index].storeName}",
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Text(
+                          payment,
+                          style: TextStyle(fontSize: 12),
+                        ),
+                      ],
+                    ),
+                    Text(
+                      state,
+                      style: TextStyle(color: Colors.red, fontSize: 15),
+                    ),
+                  ],
+                ),
               ),
-            ]),
-            isThreeLine: true,
-            onTap: () {
-              // Navigator.push(
-              //   context,
-                // MaterialPageRoute(
-                //     builder: (context) => OrderDetailScreen(
-                //       orderId: orders[index].orderId,
-                //       storeId: orders[index].storeId,
-                //     )),
-              // );
-            },
-          ),
-        );
+              Stack(
+                children: [
+                  LinearProgressIndicator(
+                    backgroundColor: Color(0xfff1f2f3),
+                    valueColor: AlwaysStoppedAnimation<Color>(Colors.blue),
+                    value: 0.2,
+                    minHeight: 20,
+                  ),
+                  Container(
+                      alignment: Alignment.centerRight,
+                      margin: EdgeInsets.only(right: 10),
+                      child: Text(
+                        "${duration.inMinutes}분",
+                        style: TextStyle(color: Colors.black),
+                      )),
+                ],
+              )
+            ]));
       },
       separatorBuilder: (BuildContext context, int index) {
         return Divider();
       },
     );
+  }
+
+  complete() {
+    _listView = ListView.separated(
+      itemCount: completeOrders.length,
+      itemBuilder: (BuildContext context, int index) {
+        String payment, elapsedTime;
+        String state;
+        String storeDong = "";
+        String deliveryDong = "";
+
+        if (completeOrders[index].payment == 0)
+          payment = "선결제";
+        else if (completeOrders[index].payment == 1)
+          payment = "현장결제 / 카드";
+        else
+          payment = "현장결제 / 현금";
+
+        if (completeOrders[index].state == 2) {
+          state = "픽업 전";
+        } else if (completeOrders[index].state == 3) {
+          state = "배달 중";
+        } else {
+          state = "${completeOrders[index].deliveryFee}원";
+        }
+
+        final storeLocationList = completeOrders[index].storeLocation.split(' ');
+        storeLocationList.forEach((element) {
+          if (element[element.length - 1] == '동') {
+            storeDong = element;
+          }
+        });
+
+        final deliveryLocationList = completeOrders[index].deliveryLocation.split(' ');
+        deliveryLocationList.forEach((element) {
+          if (element[element.length - 1] == '동') {
+            deliveryDong = element;
+          }
+        });
+
+        DateTime orderTime = DateTime.parse(completeOrders[index].orderTime);
+
+        DateTime current = DateTime.now();
+        Duration duration = current.difference(orderTime);
+        double _percent =
+            (duration.inMinutes / completeOrders[index].predictTime).toDouble();
+        if (_percent >= 1) _percent = 1;
+
+        return Container(
+            margin: EdgeInsets.all(15),
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text("${completeOrders[index].deliveryTime?.substring(0, 16)}"),
+                      Text(
+                        "${completeOrders[index].deliveryFee}원",
+                        style: TextStyle(color: Colors.blue),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Container(
+                    color: Color(0xfff1f2f3),
+                    height: 2,
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Text(
+                      "${completeOrders[index].orderInfo.hashCode.toRadixString(16).toUpperCase()}"),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "${completeOrders[index].storeName}",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      Text(
+                          "${completeOrders[index].deliveryDistance.toStringAsFixed(2)}km"),
+                    ],
+                  ),
+                  Container(
+                    alignment: Alignment.centerRight,
+                    child: Text("총 ${duration.inMinutes}분 소요"),
+                  )
+                ]));
+      },
+      separatorBuilder: (BuildContext context, int index) {
+        return Divider();
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    order_state ? proceeding() : complete();
 
     return DefaultTabController(
-        length: 2,
+        length: 1,
         child: Scaffold(
-          backgroundColor: Colors.white,
-          bottomNavigationBar: MenuBottom(userId: widget.userId, tabItem: TabItem.list),
-          appBar: AppBar(
-            title: Container(
-              child:
-              Text(
-                "오더리스트",
-                style: TextStyle(color: Colors.black),
-              ),),
-            shape: Border(
-                bottom: BorderSide(
-                  color: Color(0xfff1f2f3),
-                  width: 2,
-                )),
-            automaticallyImplyLeading: false,
-            centerTitle: true,
-            backgroundColor: Color(0xfff1f2f3),
-            elevation: 0,
-            bottom: TabBar(
-              controller: _tabController,
-              indicator: BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(8.0),
-                      topRight: Radius.circular(8.0)),
-                  color: Colors.white),
-              labelColor: Colors.black,
-              unselectedLabelColor: Colors.black,
-              tabs: _tabs,
-              onTap: (int i) {
-                if (i == 0) {
-                  tabValue = "진행 중";
-                  orderList();
-                } else {
-                  tabValue = "완료";
-                  orderList();
-                }
-              },
+            backgroundColor: Colors.white,
+            bottomNavigationBar:
+                MenuBottom(userId: widget.userId, tabItem: TabItem.list),
+            appBar: AppBar(
+              title: Container(
+                child: Text(
+                  "오더리스트",
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
+              shape: Border(
+                  bottom: BorderSide(
+                color: Color(0xfff1f2f3),
+                width: 2,
+              )),
+              automaticallyImplyLeading: false,
+              centerTitle: true,
+              backgroundColor: Color(0xfff1f2f3),
+              elevation: 0,
+              bottom: TabBar(
+                controller: _tabController,
+                indicator: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(8.0),
+                        topRight: Radius.circular(8.0)),
+                    color: Colors.white),
+                labelColor: Colors.black,
+                unselectedLabelColor: Colors.black,
+                tabs: _tabs,
+                onTap: (int i) {
+                  if (i == 0) {
+                    tabValue = "진행 중";
+                    order_state = true;
+                    proceedingOrderList();
+                  } else {
+                    tabValue = "완료";
+                    order_state = false;
+                    completeOrderList();
+                  }
+                },
+              ),
             ),
-          ),
+            //body: TabBarView(children: [_listView],
+            body: Column(
+            children: [
+              Expanded(child: TabBarView(children: [_listView]),),
+              if(!order_state && !previous)SizedBox(
 
-          body: TabBarView(children: [_listView, _listView]),
-        ));
+                height: 40,
+                width: MediaQuery.of(context).size.width,
+                child: ElevatedButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Color(0xfff1f2f3)),
+                  onPressed: () {
+                    previous = true;
+                    completeOrderList();
+                    setState(() {});
+                  }, child: Text("지난 내역 >", style: TextStyle(color: Colors.black),)),
+              )
+            ]
+        ))
+    );
   }
 }
