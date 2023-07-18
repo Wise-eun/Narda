9982596686d831f83fed813fe764676a27fd40bc
dodms <dodms1006@naver.com>
2023-07-18 10:51:27 +0900

    [Feat] 라즈베리파이와 블루투스 연동

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,17 @@
 import 'dart:async';
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_naver_map/flutter_naver_map.dart';
 import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
 import 'package:kakao_flutter_sdk_navi/kakao_flutter_sdk_navi.dart';
 import 'package:speelow/find_id_screen.dart';
 import 'package:speelow/main_screen.dart';
+import 'package:speelow/selectBondedDevicePage.dart';
 import 'package:speelow/signup_screen.dart';
 import 'package:firebase_core/firebase_core.dart' hide Order;
+import 'DiscoveryPage.dart';
 import 'after_order_list.dart';
 import 'api/api.dart';
 import 'find_pw_screen.dart';
@@ -32,9 +35,14 @@ import 'menu_bottom.dart';
 import 'model/order.dart';
 import 'model/orderDetail.dart';
 import 'order_detail.dart';
+import 'dart:async';
 
+import 'package:flutter/material.dart';
+import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
+
+import './ChatPage.dart';
+import './selectBondedDevicePage.dart';
 import 'dart:math' as math;
-
 import 'package:flutter/material.dart';
 //import 'package:flutter_hooks/flutter_hooks.dart';
 
@@ -80,6 +88,17 @@ class Loginscreen extends StatefulWidget {
 }
 
 class _LoginscreenState extends State<Loginscreen> {
+  BluetoothState _bluetoothState = BluetoothState.UNKNOWN;
+
+  String _address = "...";
+  String _name = "...";
+
+  Timer? _discoverableTimeoutTimer;
+  int _discoverableTimeoutSecondsLeft = 0;
+
+
+  bool _autoAcceptPairingRequests = false;
+
   final db = FirebaseFirestore.instance;
 
   final TextEditingController _idController =
@@ -165,9 +184,67 @@ class _LoginscreenState extends State<Loginscreen> {
         ));
   }
 
+  void _startChat(BuildContext context, BluetoothDevice server) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) {
+          return ChatPage(server: server);
+        },
+      ),
+    );
+  }
+
   void initState() {
     super.initState();
     initialization();
+
+    FlutterBluetoothSerial.instance.state.then((state) {
+      setState(() {
+        _bluetoothState = state;
+      });
+    });
+
+    Future.doWhile(() async {
+      // Wait if adapter not enabled
+      if ((await FlutterBluetoothSerial.instance.isEnabled) ?? false) {
+        return false;
+      }
+      await Future.delayed(Duration(milliseconds: 0xDD));
+      return true;
+    }).then((_) {
+      // Update the address field
+      FlutterBluetoothSerial.instance.address.then((address) {
+        setState(() {
+          _address = address!;
+        });
+      });
+    });
+
+    FlutterBluetoothSerial.instance.name.then((name) {
+      setState(() {
+        _name = name!;
+      });
+    });
+
+    // Listen for futher state changes
+    FlutterBluetoothSerial.instance
+        .onStateChanged()
+        .listen((BluetoothState state) {
+      setState(() {
+        _bluetoothState = state;
+
+        // Discoverable mode is disabled when Bluetooth gets disabled
+        _discoverableTimeoutTimer = null;
+        _discoverableTimeoutSecondsLeft = 0;
+      });
+    });
+
+  }
+
+  void dispose() {
+    FlutterBluetoothSerial.instance.setPairingRequestHandler(null);
+    _discoverableTimeoutTimer?.cancel();
+    super.dispose();
   }
 
   @override
@@ -258,6 +335,49 @@ class _LoginscreenState extends State<Loginscreen> {
                             child: Text('비밀번호 찾기',
                                 style: TextStyle(fontSize: 13, color: Colors.black))),
                       ],
+                    ),
+                    ListTile(
+                      title: ElevatedButton(
+                          child: const Text('Explore discovered devices'),
+                          onPressed: () async {
+                            final BluetoothDevice? selectedDevice =
+                            await Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (context) {
+                                  return DiscoveryPage();
+                                },
+                              ),
+                            );
+
+                            if (selectedDevice != null) {
+                              print('Discovery -> selected ' + selectedDevice.address);
+                            } else {
+                              print('Discovery -> no device selected');
+                            }
+                          }),
+                    ),
+
+                    ListTile(
+                      title: ElevatedButton(
+                        child: const Text('Connect to paired device to chat'),
+                        onPressed: () async {
+                          final BluetoothDevice? selectedDevice =
+                          await Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (context) {
+                                return SelectBondedDevicePage(checkAvailability: false);
+                              },
+                            ),
+                          );
+
+                          if (selectedDevice != null) {
+                            print('Connect -> selected ' + selectedDevice.address);
+                            _startChat(context, selectedDevice);
+                          } else {
+                            print('Connect -> no device selected');
+                          }
+                        },
+                      ),
                     ),
                     Spacer(),
                   ],
